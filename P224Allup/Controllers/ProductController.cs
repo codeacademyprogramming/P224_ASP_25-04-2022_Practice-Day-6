72f136704950baa40ee6c8ba148523172fd0aa99
Hamid V. Mammadov <Hamid.mammadov@code.local>
2022-04-25 17:45:18 +0400

    First Commit

---
+++ P224Allup/Controllers/ProductController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using P224Allup.DAL;
+using P224Allup.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P224Allup.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AllupDbContext _context;
+
+        public ProductController(AllupDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> DetailModal(int? id)
+        {
+            if (id == null) return BadRequest();
+
+            Product product = await _context.Products.Include(p=>p.ProductImages).FirstOrDefaultAsync(p => p.Id == id && !p.IsDeleted);
+
+            if (product == null) return NotFound();
+
+            return PartialView("_ProductDetailPartial",product);
+        }
+
+        public async Task<IActionResult> AddBasket(int? id, int count = 1)
+        {
+            if (id == null) return BadRequest();
+
+            Product product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
+
+            if (product == null) return NotFound();
+            
+
+            return PartialView("_BasketPartial");
+        }
+    }
+}
---
+++ P224Allup/wwwroot/assets/js/custom.js
@@ -0,0 +1,46 @@
+﻿$(document).ready(function () {
+    $(document).on("click", "#productdetail", function (e) {
+        e.preventDefault();
+
+        let url = $(this).attr("href");
+
+        fetch(url).then(response => response.text())
+            .then(data => {
+                $(".modal-content").html(data)
+
+                $('.quick-view-image').slick({
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    dots: false,
+                    fade: true,
+                    asNavFor: '.quick-view-thumb',
+                    speed: 400,
+                });
+
+                $('.quick-view-thumb').slick({
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    asNavFor: '.quick-view-image',
+                    dots: false,
+                    arrows: false,
+                    focusOnSelect: true,
+                    speed: 400,
+                });
+
+                $("#productQuickModal").modal("show")
+            })
+    })
+
+    $(document).on("click", "#addbasketbtn", function (e) {
+        e.preventDefault()
+
+        let url = $("#basketform").attr("action")
+        let count = $("#productcount").val();
+        url = url + "?count=" + count;
+        fetch(url).then(response =>
+        {
+            return response.json();
+        }).then(data => console.log(data))
+    })
+})
